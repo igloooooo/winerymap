8465ae8a38eb276288319dbf28f50584b89abe18
igloooooo <nichols.zu@gmail.com>
2013-04-02 13:22:00 +1100

    listview update

--- winerymap/src/au/com/iglooit/winerymap/android/view/home/HomePageViewAdapter.java
+++ winerymap/src/au/com/iglooit/winerymap/android/view/home/HomePageViewAdapter.java
@@ -5,40 +5,39 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import java.util.ArrayList;
+
 public class HomePageViewAdapter extends FragmentPagerAdapter
 {
 
     private Context _context;
+    private ArrayList<Fragment> fragments;
 
     public HomePageViewAdapter(Context context, FragmentManager fm)
     {
         super(fm);
         _context = context;
-
+        fragments = new ArrayList<Fragment>();
+        fragments.add(NewsFragment.newInstance(_context));
+        fragments.add(MapSearchFragment.newInstance(_context));
+        fragments.add(SearchDetailsFragment.newInstance(_context));
     }
 
     @Override
     public Fragment getItem(int position)
     {
-        Fragment f = new Fragment();
-        switch (position)
-        {
-            case 0:
-                f = NewsFragment.newInstance(_context);
-                break;
-            case 1:
-                f = MapSearchFragment.newInstance(_context);
-                break;
-            case 2:
-                f = SearchDetailsFragment.newInstance(_context);
-                break;
-        }
-        return f;
+        return fragments.get(position);
     }
 
     @Override
     public int getCount()
     {
-        return 3;
+        return fragments.size();
+    }
+
+    @Override
+    public int getItemPosition(Object object)
+    {
+        return super.getItemPosition(object);
     }
 }
--- winerymap/src/au/com/iglooit/winerymap/android/view/home/NewsFragment.java
+++ winerymap/src/au/com/iglooit/winerymap/android/view/home/NewsFragment.java
@@ -10,10 +10,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import au.com.iglooit.winerymap.android.R;
 import au.com.iglooit.winerymap.android.dbhelper.DataHelper;
-import au.com.iglooit.winerymap.android.dbhelper.WineryInfoHelper;
 import com.androidquery.AQuery;
 import com.handmark.pulltorefresh.extras.listfragment.PullToRefreshListFragment;
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
@@ -77,92 +77,7 @@ public class NewsFragment extends Fragment implements PullToRefreshBase.OnRefres
     @Override
     public void onActivityCreated(Bundle savedInstanceState)
     {
-        super.onActivityCreated(savedInstanceState);    //To change body of overridden methods use File | Settings |
-
-        mPullRefreshListFragment = (PullToRefreshListFragment)this.getActivity().getSupportFragmentManager()
-            .findFragmentById(R.id.latest_news_list);
-
-        // Get PullToRefreshListView from Fragment
-        mPullRefreshListView = mPullRefreshListFragment.getPullToRefreshListView();
-
-        // Set a listener to be invoked when the list should be refreshed.
-        mPullRefreshListView.setOnRefreshListener(this);
-
-        // You can also just use mPullRefreshListFragment.getListView()
-        ListView actualListView = mPullRefreshListView.getRefreshableView();
-
-        mListItems = new LinkedList<String>();
-        mListItems.addAll(Arrays.asList(mStrings));
-        mAdapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, mListItems);
-
-        // You can also just use setListAdapter(mAdapter) or
-        // mPullRefreshListView.setAdapter(mAdapter)
-        actualListView.setAdapter(mAdapter);
-
-        mPullRefreshListFragment.setListShown(true);
-
-//        data = new ArrayList<String>();
-//        data.add("a");
-//        data.add("b");
-//        data.add("c");
-//
-//        final ScrollableListView listView = (ScrollableListView)root.findViewById(R.id.resultList_news);
-//        adapter = new BaseAdapter()
-//        {
-//            public View getView(int position, View convertView, ViewGroup parent)
-//            {
-//                TextView tv = new TextView(getActivity());
-//                tv.setText(data.get(position));
-//                return tv;
-//            }
-//
-//            public long getItemId(int position)
-//            {
-//                return 0;
-//            }
-//
-//            public Object getItem(int position)
-//            {
-//                return null;
-//            }
-//
-//            public int getCount()
-//            {
-//                return data.size();
-//            }
-//        };
-//        listView.setAdapter(adapter);
-//
-//        listView.setonRefreshListener(new ScrollableListView.OnRefreshListener()
-//        {
-//            public void onRefresh()
-//            {
-//                new AsyncTask<Void, Void, Void>()
-//                {
-//                    protected Void doInBackground(Void... params)
-//                    {
-//                        try
-//                        {
-//                            Thread.sleep(1000);
-//                        }
-//                        catch (Exception e)
-//                        {
-//                            e.printStackTrace();
-//                        }
-//                        data.add("new data after refresh");
-//                        return null;
-//                    }
-//
-//                    @Override
-//                    protected void onPostExecute(Void result)
-//                    {
-//                        adapter.notifyDataSetChanged();
-//                        listView.onRefreshComplete();
-//                    }
-//
-//                }.execute(null);
-//            }
-//        });
+        super.onActivityCreated(savedInstanceState);
     }
 
     @Override
@@ -171,27 +86,7 @@ public class NewsFragment extends Fragment implements PullToRefreshBase.OnRefres
 
         root = (ViewGroup)inflater.inflate(R.layout.wm_news_fragment, container, false);
         aq = new AQuery(root);
-//        mListView = (ListView)root.findViewById(R.id.resultList_news);
-
-//        String[] mFrom = new String[]{"img", "title1", "title2", "time"};
-//        int[] mTo = new int[]{R.id.img, R.id.title1, R.id.title2, R.id.time};
-//        // get data from database
-//        List<WineryInfo> resultList = getDataHelper().findWineryByName("This");
-//
-//        Map<String, Object> mMap = null;
-//
-//        mMap = new HashMap<String, Object>();
-//        mMap.put("img", R.drawable.ic_launcher);
-//        mMap.put("title1", 0);
-//        mMap.put("title2", "Remove All");
-//        mMap.put("time", "2011-08-15 09:00");
-//        mList.add(mMap);
-//
-//        mAdapter = new SearchDetailsBarAdapter(root.getContext(), mList, R.layout.wm_home_search_result_list_item,
-//            mFrom, mTo);
-//        mListView.setAdapter(mAdapter);
-
-
+        initContent();
         return root;
     }
 
@@ -230,6 +125,16 @@ public class NewsFragment extends Fragment implements PullToRefreshBase.OnRefres
 
             super.onPostExecute(result);
         }
+    }
+
+    private void initContent()
+    {
+        PullToRefreshListView plv = (PullToRefreshListView)root.findViewById(R.id.latest_news_listview);
+        ListAdapter adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1,
+            Arrays.asList(mStrings));
+        plv.setAdapter(adapter);
+
+        plv.setOnRefreshListener(this);
     }
 
     private String[] mStrings = {"Abbaye de Belloc", "Abbaye du Mont des Cats", "Abertam", "Abondance", "Ackawi",
